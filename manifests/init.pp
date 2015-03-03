30b538bfb777a56f2a8327d8c6d5d4309aaa2ca2
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-03-03 13:54:14 -0500

    install consul-replicate

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,21 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class consul_replicate {
+class consul_replicate (
+ $version = "0.2.0"
+){
 
-
+staging::file { 'consul-replicate.tar.gz':
+  source => "https://github.com/hashicorp/consul-replicate/releases/download/v0.2.0/consul-replicate_$version_linux_amd64.tar.gz"
+} ->
+staging::extract { 'consul-replicate.tar.gz':
+  strip   => 1,
+  target  => "/usr/local/bin",
+  creates => "/usr/local/bin/consul-replicate",
+} ->
+file { "/usr/local/bin/consul-replicate":
+  owner =>  0,
+  group =>  0,
+  mode  => '0555',
+}
 }
